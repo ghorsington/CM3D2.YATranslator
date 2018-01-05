27d35b754ee9b12acd4da946940c03c20329bbbd
denikson <geoffrey.hoooooorse@gmail.com>
2018-01-05 19:12:56 +0200

    new branch

--- CM3D2.YATranslator.Hook/SoundEventArgs.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace CM3D2.YATranslator.Hook
-{
-    public class SoundEventArgs : EventArgs
-    {
-        public AudioSourceMgr AudioSourceMgr { get; internal set; }
-    }
-}
